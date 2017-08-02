a4a30e39a84234142df6b29ce0ab2b6f7ef03856
khai96_ <hvksmr1996@gmail.com>
2017-08-03 05:39:48 +0700

    Fix indent style

--- index.js
+++ index.js
@@ -23,33 +23,33 @@ function main ({
   )
 
   return [
-      ['dependencies', 'save-prod'],
-      ['bundleDependencies', 'save-bundle'],
-      ['optionalDependencies', 'save-optional'],
-      ['devDependencies', 'save-dev']
+    ['dependencies', 'save-prod'],
+    ['bundleDependencies', 'save-bundle'],
+    ['optionalDependencies', 'save-optional'],
+    ['devDependencies', 'save-dev']
   ]
-      .map(([name, save]) => [info[name], save, name])
-      .filter(pair => pair[0])
-      .map(([object, save, name]) => [Object.getOwnPropertyNames(object), save, name])
-      .map(NPM_TAG
-        ? ([packages, ...rest]) => [packages.map(pkgname => pkgname + '@' + NPM_TAG), ...rest]
-        : x => x
-      )
-      .filter(([{length}]) => length)
-      .map(([packages, save, name]) => [
-        `npm install --${save} ${packages.join(' ')}`,
-        name,
-        packages.length
-      ])
-      .map(([command, name, count]) => [
-        `echo '[INFO] ${count} ${name}'`,
-        'echo [COMMAND] ' + command,
-        `cd '${wdir}' && ${command}`,
-        'echo "[STATUS] $?"',
-        '\n'
-      ])
-      .map(cmdset => cmdset.join('\n'))
-      .join('')
+    .map(([name, save]) => [info[name], save, name])
+    .filter(pair => pair[0])
+    .map(([object, save, name]) => [Object.getOwnPropertyNames(object), save, name])
+    .map(NPM_TAG
+      ? ([packages, ...rest]) => [packages.map(pkgname => pkgname + '@' + NPM_TAG), ...rest]
+      : x => x
+    )
+    .filter(([{length}]) => length)
+    .map(([packages, save, name]) => [
+      `npm install --${save} ${packages.join(' ')}`,
+      name,
+      packages.length
+    ])
+    .map(([command, name, count]) => [
+      `echo '[INFO] ${count} ${name}'`,
+      'echo [COMMAND] ' + command,
+      `cd '${wdir}' && ${command}`,
+      'echo "[STATUS] $?"',
+      '\n'
+    ])
+    .map(cmdset => cmdset.join('\n'))
+    .join('')
 }
 
 if (require.main === module) {

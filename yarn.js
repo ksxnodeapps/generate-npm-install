1ff401e134031bd706f69a0ca22883586a3ee9a8
khai96_ <hvksmr1996@gmail.com>
2018-03-31 20:40:42 +0700

    Use stdio: inherit

--- yarn.js
+++ yarn.js
@@ -8,9 +8,8 @@ const child = spawn('node', [require.resolve('./index.js')], {
   env: {
     ...env,
     USE_YARN: 'true'
-  }
+  },
+  stdio: 'inherit'
 })
 
-child.stdout.on('data', x => stdout.write(x))
-child.stderr.on('data', x => stderr.write(x))
 child.on('exit', x => exit(x))
